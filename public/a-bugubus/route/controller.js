f249f6814443c769409afdb3b69fd9392ef1a7f6
Guohao0509 <277482458@qq.com>
2017-11-07 15:43:04 +0800

    update by Guohao

--- public/a-bugubus/route/controller.js
+++ public/a-bugubus/route/controller.js
@@ -28,7 +28,7 @@ app.controller('RouteEditController',['$compile','$rootScope','$scope','$http','
                     lng:stations[i].stalongitude,
                     lat:stations[i].stalatitude,
                     drivingTime:stations[i].drivingtime,
-                    "stationType": $scope.stations[i].stationType
+                    "stationType":stations[i].stationType
                 })
             };
             window.setTimeout(function(){
