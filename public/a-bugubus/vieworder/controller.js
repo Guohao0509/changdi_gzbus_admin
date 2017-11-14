bc41fb7abaac2b13b4883c197fa313c379d7ad44
Guohao0509 <277482458@qq.com>
2017-11-14 09:11:28 +0800

    add viewOrder searching viewOrder by newTime function by Guohao

--- public/a-bugubus/vieworder/controller.js
+++ public/a-bugubus/vieworder/controller.js
@@ -1,6 +1,6 @@
 /**
  * @author Guohao
- * @date 2016-10-18
+ * @date 2017-11-18
  * @version 1.0.0
  * @descriptions 景区订单管理的控制器
  */
@@ -12,4 +12,28 @@ app.controller('ViewOrderController',['$scope','$http','$state','$myHttpService'
     $tableListService.init($scope, options);
     $tableListService.get();
     console.log($scope);
+
+    $scope.viewOrder = {
+        opened:false,
+        dateOptions:{
+            datepickerMode:'day',
+            showWeeks: false,
+            minMode:'day'
+        },
+        format:"yyyy-MM-dd",
+        clear:function(){
+            $scope.newTime = null;
+        },
+        disabled:function(date, mode) {
+            return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+        },
+        toggleMin: function() {
+            $scope.minDate = $scope.minDate ? null : new Date();
+        },
+        open:function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.viewOrder.opened = true;
+        }
+    };
 }]);
